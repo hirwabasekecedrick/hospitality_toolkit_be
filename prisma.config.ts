522077641919ceacb5ceb55b2583ba88d83b7425
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-06-05 18:17:09 +0200

    Adding staging to the repo

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: "postgresql://postgres:123@localhost:5432/convenience_card",
+    url: 'postgresql://neondb_owner:npg_UvpqmDg6K4dG@ep-holy-cloud-ap3tuxyo-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   },
 });
